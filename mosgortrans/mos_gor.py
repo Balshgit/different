965244501f6d6370af276b9703a6f4ec717bfb29
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 01:50:42 +0000

    Adding tarfile member sanitization to extractall()

--- mosgortrans/mos_gor.py
+++ mosgortrans/mos_gor.py
@@ -60,7 +60,26 @@ def download_gecko_driver():
         geckodriver_file = wget.download(url=gecko_driver, out=BASE_DIR)
 
         with tarfile.open(geckodriver_file) as tar:
-            tar.extractall(BASE_DIR)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, BASE_DIR)
         os.remove(f'{BASE_DIR}/geckodriver-v{GECKO_DRIVER_VERSION}-linux64.tar.gz')
         logger.info(f'\ngeckodriver has been downloaded to folder {BASE_DIR}')
 
